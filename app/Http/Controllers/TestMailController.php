55323a32c94c0d77496cfe26aecd180a256d4235
ibrataha8 <ibrataha8@gmail.com>
2023-08-29 14:03:12 +0100

    add mail send

--- app/Http/Controllers/TestMailController.php
+++ app/Http/Controllers/TestMailController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\TestMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class TestMailController extends Controller
 {
-    //
+    public function testMailF()
+    {
+        Mail::to("haland.hulk64@gmail.com
+        ")->send(new TestMail());
+        echo "Message sent";
+    }
 }
